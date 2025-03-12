ethpandaops/optimism-package

commit d137619ec1e3677366dc3198a16390b68026153d

fix: `kt run .` without --args-file should run without error (#166)
Fix the defaults in input_parser and sanity_checker such that `kt run .`
works without errors.
Also added running `kt run .` without --args-file to per-pr ci file so
that we can catch these errors early (for eg there was a bug in my
previous altda default config such that it wasnt even getting picked
up).


I like this quote from
https://chshersh.com/blog/2025-01-06-the-most-elegant-configuration-language.html
<img width="813" alt="image"
src="https://github.com/user-attachments/assets/6fef77f8-a58f-4dfa-b8d6-3c866ffc541c"
/>

main.star
@@ -39,7 +39,9 @@ def run(plan, args):
 
     # need to do a raw get here in case only optimism_package is provided.
     # .get will return None if the key is in the config with a None value.
-    optimism_args = args.get("optimism_package") or input_parser.default_optimism_args()
+    optimism_args = (
+        args.get("optimism_package") or input_parser.default_optimism_params()
+    )
     optimism_args_with_right_defaults = input_parser.input_parser(plan, optimism_args)
     global_tolerations = optimism_args_with_right_defaults.global_tolerations
     global_node_selectors = optimism_args_with_right_defaults.global_node_selectors

.github/workflows/per-pr.yml
@@ -26,6 +26,23 @@ jobs:
       - name: Run Starlark
         run: kurtosis run ${{ github.workspace }} --args-file ${{ matrix.file_name }} --verbosity detailed
 
+  # Make sure that `kurtosis run .` without an --args-file works fine (the defaults in input_parser.star are correct)
+  run_without_args:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Repository
+        uses: actions/checkout@v4
+
+      - name: Setup Kurtosis
+        run: |
+          echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
+          sudo apt update
+          sudo apt install kurtosis-cli
+          kurtosis analytics disable
+
+      - name: Run Starlark
+        run: kurtosis run ${{ github.workspace }} --verbosity detailed
+
   lint:
     runs-on: ubuntu-latest
     steps:

src/package_io/input_parser.star
@@ -656,6 +656,12 @@ def default_op_contract_deployer_params():
 
 def default_ethereum_package_network_params():
     return {
+        "participants": [
+            {
+                "el_type": "geth",
+                "cl_type": "teku",
+            }
+        ],
         "network_params": {
             "preset": "minimal",
             "genesis_delay": 5,
@@ -670,7 +676,7 @@ def default_ethereum_package_network_params():
                     }
                 }
             ),
-        }
+        },
     }
 
 

src/package_io/sanity_check.star
@@ -102,8 +102,28 @@ PARTICIPANT_CATEGORIES = {
         "cl_max_mem",
         "el_builder_type",
         "el_builder_image",
+        "el_builder_log_level",
+        "el_builder_extra_env_vars",
+        "el_builder_extra_labels",
+        "el_builder_extra_params",
+        "el_builder_tolerations",
+        "el_builder_volume_size",
+        "el_builder_min_cpu",
+        "el_builder_max_cpu",
+        "el_builder_min_mem",
+        "el_builder_max_mem",
         "cl_builder_type",
         "cl_builder_image",
+        "cl_builder_log_level",
+        "cl_builder_extra_env_vars",
+        "cl_builder_extra_labels",
+        "cl_builder_extra_params",
+        "cl_builder_tolerations",
+        "cl_builder_volume_size",
+        "cl_builder_min_cpu",
+        "cl_builder_max_cpu",
+        "cl_builder_min_mem",
+        "cl_builder_max_mem",
         "node_selectors",
         "tolerations",
         "count",
@@ -135,6 +155,7 @@ SUBCATEGORY_PARAMS = {
     ],
     "mev_params": ["rollup_boost_image", "builder_host", "builder_port"],
     "da_server_params": [
+        "enabled",
         "image",
         "cmd",
     ],

commit a8d31b4f1d0c7752f021edd326db1372f88978f8

fix: default optimism_args to empty (#196)
This PR defaults the `optimism_package` configuration block to an empty
dictionary if none is provided by the user in order to fix an issue
introduced in https://github.com/ethpandaops/optimism-package/pull/166.

main.star
@@ -37,18 +37,15 @@ def run(plan, args={}):
         if "network_params" not in ethereum_args:
             ethereum_args.update(input_parser.default_ethereum_package_network_params())
 
-    # need to do a raw get here in case only optimism_package is provided.
-    # .get will return None if the key is in the config with a None value.
-    optimism_args = args.get("optimism_package") or {}
-    optimism_args_with_right_defaults = input_parser.input_parser(plan, optimism_args)
-    global_tolerations = optimism_args_with_right_defaults.global_tolerations
-    global_node_selectors = optimism_args_with_right_defaults.global_node_selectors
-    global_log_level = optimism_args_with_right_defaults.global_log_level
-    persistent = optimism_args_with_right_defaults.persistent
-    altda_deploy_config = optimism_args_with_right_defaults.altda_deploy_config
+    optimism_args = input_parser.input_parser(plan, args.get("optimism_package", {}))
+    global_tolerations = optimism_args.global_tolerations
+    global_node_selectors = optimism_args.global_node_selectors
+    global_log_level = optimism_args.global_log_level
+    persistent = optimism_args.persistent
+    altda_deploy_config = optimism_args.altda_deploy_config
 
-    observability_params = optimism_args_with_right_defaults.observability
-    interop_params = optimism_args_with_right_defaults.interop
+    observability_params = optimism_args.observability
+    interop_params = optimism_args.interop
 
     observability_helper = observability.make_helper(observability_params)
 
@@ -94,7 +91,7 @@ def run(plan, args={}):
         plan,
         l1_priv_key,
         l1_config_env_vars,
-        optimism_args_with_right_defaults,
+        optimism_args,
         l1_network,
         altda_deploy_config,
     )
@@ -105,7 +102,7 @@ def run(plan, args={}):
     )
 
     l2s = []
-    for l2_num, chain in enumerate(optimism_args_with_right_defaults.chains):
+    for l2_num, chain in enumerate(optimism_args.chains):
         l2s.append(
             l2_launcher.launch_l2(
                 plan,
@@ -130,7 +127,7 @@ def run(plan, args={}):
         op_supervisor_launcher.launch(
             plan,
             l1_config_env_vars,
-            optimism_args_with_right_defaults.chains,
+            optimism_args.chains,
             l2s,
             jwt_file,
             interop_params.supervisor_params,
@@ -139,7 +136,7 @@ def run(plan, args={}):
 
     # challenger must launch after supervisor because it depends on it for interop
     for l2_num, l2 in enumerate(l2s):
-        chain = optimism_args_with_right_defaults.chains[l2_num]
+        chain = optimism_args.chains[l2_num]
         op_challenger_image = (
             chain.challenger_params.image
             if chain.challenger_params.image != ""