ethpandaops/optimism-package

commit f9b3519a641c245db562579e328dc75c32382152

feat: Per-interop-set supervisor (#220)
**Description**

Adds a concept of interop sets so that `op-supervisor` can be set up
with more nuance.

Interop sets get their configuration section under `interop.sets`. A
single interop set binds several L2 chains (referenced by their network
IDs) into an interoperable system.

- The `participants` field refers to network IDs of the L2 chains.
Special value `*` can be used to quickly assign all L2s into a single
interop set. To simulate misconfiguration, a single L2 can be assigned
to multiple interop sets
- `supervisor_params` can override default supervisor parameters under
`interop.supervisor_params`
- Interop is enabled if there is at least one interop set. It can be
disabled explicitly either as a whole or per-interop-set using
`interop.enabled` and `interop.sets[].enabled` boolean fields

main.star
@@ -124,15 +124,15 @@ def run(plan, args={}):
         )
 
     if interop_params.enabled:
-        op_supervisor_launcher.launch(
-            plan,
-            l1_config_env_vars,
-            optimism_args.chains,
-            l2s,
-            jwt_file,
-            interop_params.supervisor_params,
-            observability_helper,
-        )
+        for i, interop_set in enumerate(interop_params.sets):
+            op_supervisor_launcher.launch(
+                plan=plan,
+                interop_set=interop_set,
+                l1_config_env_vars=l1_config_env_vars,
+                l2s=l2s,
+                jwt_file=jwt_file,
+                observability_helper=observability_helper,
+            )
 
     # challenger must launch after supervisor because it depends on it for interop
     for l2_num, l2 in enumerate(l2s):

src/interop/constants.star
@@ -1,4 +1,3 @@
-constants = import_module("../package_io/constants.star")
 util = import_module("../util.star")
 
 INTEROP_WS_PORT_ID = "interop-ws"

src/interop/op-supervisor/op_supervisor_launcher.star
@@ -8,10 +8,10 @@ ethereum_package_constants = import_module(
     "github.com/ethpandaops/ethereum-package/src/package_io/constants.star"
 )
 
-constants = import_module("../../package_io/constants.star")
 observability = import_module("../../observability/observability.star")
 prometheus = import_module("../../observability/prometheus/prometheus_launcher.star")
 
+constants = import_module("../../package_io/constants.star")
 interop_constants = import_module("../constants.star")
 
 
@@ -27,18 +27,21 @@ def get_used_ports():
 
 
 DATA_DIR = "/etc/op-supervisor"
-DEPENDENCY_SET_FILE_NAME = "dependency_set.json"
 
 
-def create_dependency_set(chains):
+def create_dependency_set_filename(interop_set):
+    return "dependency_set-{}.json".format(interop_set.name)
+
+
+def create_dependency_set(l2s):
     result = {
         "dependencies": {
-            str(chain.network_params.network_id): {
-                "chainIndex": str(chain.network_params.network_id),
+            str(l2.network_id): {
+                "chainIndex": str(l2.network_id),
                 "activationTime": 0,
                 "historyMinTime": 0,
             }
-            for chain in chains
+            for l2 in l2s
         }
     }
     return result
@@ -46,40 +49,68 @@ def create_dependency_set(chains):
 
 def launch(
     plan,
+    interop_set,
     l1_config_env_vars,
-    chains,
     l2s,
     jwt_file,
-    supervisor_params,
     observability_helper,
 ):
-    dependency_set_json = supervisor_params.dependency_set
-    if not dependency_set_json:
-        dependency_set = create_dependency_set(chains)
-        dependency_set_json = json.encode(dependency_set)
+    # First we check that the supervisor is enabled for this interop set
+    if not interop_set.enabled:
+        plan.print(
+            "op-supervisor is not enabled for interop set {}, skipping launch".format(
+                interop_set.name
+            )
+        )
+        return None
 
+    # Then we check that we have some participants
+    if len(interop_set.participants) == 0:
+        plan.print(
+            "op-supervisor has no participants for interop set {}, skipping launch".format(
+                interop_set.name
+            )
+        )
+        return None
+
+    # Now we filter out the participating L2s
+    interop_set_l2s = [l2 for l2 in l2s if l2.network_id in interop_set.participants]
+
+    # Now we create dependency set if none was provided
+    dependency_set_json = interop_set.supervisor_params.dependency_set or json.encode(
+        create_dependency_set(interop_set_l2s)
+    )
+
+    # And write it to an artifact
+    dependency_set_filename = create_dependency_set_filename(interop_set)
     dependency_set_artifact = utils.write_to_file(
-        plan, dependency_set_json, DATA_DIR, DEPENDENCY_SET_FILE_NAME
+        plan, dependency_set_json, DATA_DIR, dependency_set_filename
+    )
+
+    # We create a service name based on the interop set name
+    service_name = "{}-{}".format(
+        interop_constants.SUPERVISOR_SERVICE_NAME, interop_set.name
     )
 
     config = get_supervisor_config(
-        plan,
-        l1_config_env_vars,
-        l2s,
-        jwt_file,
-        dependency_set_artifact,
-        supervisor_params,
-        observability_helper,
+        plan=plan,
+        l1_config_env_vars=l1_config_env_vars,
+        l2s=interop_set_l2s,
+        jwt_file=jwt_file,
+        dependency_set_artifact=dependency_set_artifact,
+        dependency_set_filename=dependency_set_filename,
+        supervisor_params=interop_set.supervisor_params,
+        observability_helper=observability_helper,
     )
 
-    service = plan.add_service(interop_constants.SUPERVISOR_SERVICE_NAME, config)
+    service = plan.add_service(service_name, config)
 
     observability.register_op_service_metrics_job(
         observability_helper,
         service,
     )
 
-    return "op_supervisor"
+    return service
 
 
 def get_supervisor_config(
@@ -88,6 +119,7 @@ def get_supervisor_config(
     l2s,
     jwt_file,
     dependency_set_artifact,
+    dependency_set_filename,
     supervisor_params,
     observability_helper,
 ):
@@ -110,7 +142,7 @@ def get_supervisor_config(
         env_vars={
             "OP_SUPERVISOR_DATADIR": "/db",
             "OP_SUPERVISOR_DEPENDENCY_SET": "{0}/{1}".format(
-                DATA_DIR, DEPENDENCY_SET_FILE_NAME
+                DATA_DIR, dependency_set_filename
             ),
             "OP_SUPERVISOR_L1_RPC": l1_config_env_vars["L1_RPC_URL"],
             "OP_SUPERVISOR_L2_CONSENSUS_NODES": ",".join(

commit e76bd109f8fa0e8b48336f9b14a073df184c8ed0

fix: Fix challenger <> supervisor interaction (#230)
**Description**

This PR fixes the challenger <> supervisor communication broken by #220.
The system has become more complex and constant `SUPERVISOR_ENDPOINT` is
no longer sufficient.

This constant is still being used in several places - besides `op-geth`
though its usages can be removed to (almost) break the circular
dependency between `el` <> `supervisor`

Relates to https://github.com/ethereum-optimism/optimism/issues/15151

main.star
@@ -101,62 +101,100 @@ def run(plan, args={}):
         name="op_jwt_file",
     )
 
-    l2s = []
-    for l2_num, chain in enumerate(optimism_args.chains):
-        l2s.append(
-            l2_launcher.launch_l2(
-                plan,
-                l2_num,
-                chain.network_params.name,
-                chain,
-                jwt_file,
-                deployment_output,
-                l1_config_env_vars,
-                l1_priv_key,
-                l1_rpc_url,
-                global_log_level,
-                global_node_selectors,
-                global_tolerations,
-                persistent,
-                observability_helper,
-                interop_params,
-            )
+    l2s = [
+        l2_launcher.launch_l2(
+            plan,
+            l2_num,
+            chain.network_params.name,
+            chain,
+            jwt_file,
+            deployment_output,
+            l1_config_env_vars,
+            l1_priv_key,
+            l1_rpc_url,
+            global_log_level,
+            global_node_selectors,
+            global_tolerations,
+            persistent,
+            observability_helper,
+            interop_params,
         )
+        for l2_num, chain in enumerate(optimism_args.chains)
+    ]
 
+    supervisors = []
     if interop_params.enabled:
-        for i, interop_set in enumerate(interop_params.sets):
-            op_supervisor_launcher.launch(
-                plan=plan,
-                interop_set=interop_set,
-                l1_config_env_vars=l1_config_env_vars,
-                l2s=l2s,
-                jwt_file=jwt_file,
-                observability_helper=observability_helper,
-            )
+        # We collect the output of the supervisor launcher since it  will be used by the challenger
+        supervisors = [
+            supervisor
+            # Since starlark does not support the walrus operator, we need to use a nested list comprehension
+            #
+            # TODO We do this since launcher can return None, maybe it's worth removing all the disabled values from the list beforehand
+            for supervisor in [
+                op_supervisor_launcher.launch(
+                    plan=plan,
+                    interop_set=interop_set,
+                    l1_config_env_vars=l1_config_env_vars,
+                    l2s=l2s,
+                    jwt_file=jwt_file,
+                    observability_helper=observability_helper,
+                )
+                for interop_set in interop_params.sets
+            ]
+            if supervisor != None
+        ]
 
     # challenger must launch after supervisor because it depends on it for interop
     for l2_num, l2 in enumerate(l2s):
         chain = optimism_args.chains[l2_num]
+
+        # Nothing happens if the challenger is not enabled
+        if not chain.challenger_params.enabled:
+            continue
+
         op_challenger_image = (
             chain.challenger_params.image
             if chain.challenger_params.image != ""
             else input_parser.DEFAULT_CHALLENGER_IMAGES["op-challenger"]
         )
-        if chain.challenger_params.enabled:
-            op_challenger_launcher.launch(
-                plan,
-                l2_num,
-                "op-challenger-{0}".format(chain.network_params.name),
-                chain.challenger_params.image,
-                l2.participants[0].el_context,
-                l2.participants[0].cl_context,
-                l1_config_env_vars,
-                deployment_output,
-                chain.network_params,
-                chain.challenger_params,
-                interop_params,
-                observability_helper,
+
+        # We now need to find the supervisor service for the current L2
+        #
+        # Since we allow multiple supervisors to be registered for the same L2 (to allow for intentional misconfiguration),
+        # we need to find all the supervisors for the current L2, then we pick the first one
+        l2_supervisors = [
+            supervisor
+            for supervisor in supervisors
+            if chain.network_params.network_id
+            in [n.network_id for n in supervisor.networks]
+        ]
+
+        # If there are multiple supervisors, we just let the user know
+        num_l2_supervisors = len(l2_supervisors)
+        if num_l2_supervisors > 1:
+            plan.print(
+                "Found more than one ({0} to be precise) supervisor for network {1}. Only the first one will be used for the challenger".format(
+                    num_l2_supervisors, l2.network_id
+                )
             )
+
+        # We pick the first supervisor
+        l2_supervisor = l2_supervisors[0] if num_l2_supervisors > 0 else None
+
+        op_challenger_launcher.launch(
+            plan=plan,
+            l2_num=l2_num,
+            service_name="op-challenger-{0}".format(l2.network_id),
+            image=op_challenger_image,
+            el_context=l2.participants[0].el_context,
+            cl_context=l2.participants[0].cl_context,
+            l1_config_env_vars=l1_config_env_vars,
+            deployment_output=deployment_output,
+            network_params=chain.network_params,
+            challenger_params=chain.challenger_params,
+            supervisor=l2_supervisor,
+            observability_helper=observability_helper,
+        )
 
     observability.launch(
         plan, observability_helper, global_node_selectors, observability_params

network_params.yaml
@@ -53,6 +53,10 @@ optimism_package:
   global_node_selectors: {}
   global_tolerations: []
   persistent: false
+  interop:
+    sets:
+      - name: "interop"
+        participants: "*"
 ethereum_package:
   participants:
   - el_type: geth

src/challenger/op-challenger/op_challenger_launcher.star
@@ -9,6 +9,7 @@ ethereum_package_constants = import_module(
 observability = import_module("../../observability/observability.star")
 prometheus = import_module("../../observability/prometheus/prometheus_launcher.star")
 
+constants = import_module("../../package_io/constants.star")
 interop_constants = import_module("../../interop/constants.star")
 util = import_module("../../util.star")
 
@@ -34,22 +35,22 @@ def launch(
     deployment_output,
     network_params,
     challenger_params,
-    interop_params,
+    supervisor,
     observability_helper,
 ):
     config = get_challenger_config(
-        plan,
-        l2_num,
-        service_name,
-        image,
-        el_context,
-        cl_context,
-        l1_config_env_vars,
-        deployment_output,
-        network_params,
-        challenger_params,
-        interop_params,
-        observability_helper,
+        plan=plan,
+        l2_num=l2_num,
+        service_name=service_name,
+        image=image,
+        el_context=el_context,
+        cl_context=cl_context,
+        l1_config_env_vars=l1_config_env_vars,
+        deployment_output=deployment_output,
+        network_params=network_params,
+        challenger_params=challenger_params,
+        supervisor=supervisor,
+        observability_helper=observability_helper,
     )
 
     service = plan.add_service(service_name, config)
@@ -72,7 +73,7 @@ def get_challenger_config(
     deployment_output,
     network_params,
     challenger_params,
-    interop_params,
+    supervisor,
     observability_helper,
 ):
     ports = dict(get_used_ports())
@@ -123,8 +124,12 @@ def get_challenger_config(
     if observability_helper.enabled:
         observability.configure_op_service_metrics(cmd, ports)
 
-    if interop_params.enabled:
-        cmd.append("--supervisor-rpc=" + interop_constants.SUPERVISOR_ENDPOINT)
+    if supervisor != None:
+        cmd.append(
+            "--supervisor-rpc={0}".format(
+                supervisor.service.ports[constants.RPC_PORT_ID].url
+            )
+        )
         # TraceTypeSupper{Cannon|Permissioned} needs --cannon-depset-config to be set
         # Added at https://github.com/ethereum-optimism/optimism/pull/14666
         # Temporary fix: Add a dummy flag

src/interop/constants.star
@@ -6,6 +6,7 @@ INTEROP_WS_PORT_NUM = 9645
 SUPERVISOR_SERVICE_NAME = "op-supervisor"
 SUPERVISOR_RPC_PORT_NUM = 8545
 
+# FIXME This endpoint no longer exists and its usages should be dropped
 SUPERVISOR_ENDPOINT = util.make_http_url(
     SUPERVISOR_SERVICE_NAME, SUPERVISOR_RPC_PORT_NUM
 )

src/interop/op-supervisor/op_supervisor_launcher.star
@@ -110,7 +110,10 @@ def launch(
         service,
     )
 
-    return service
+    return struct(
+        service=service,
+        networks=interop_set_l2s,
+    )
 
 
 def get_supervisor_config(

test/interop/op-supervisor/op_supervisor_launcher_test.star
@@ -340,6 +340,97 @@ def test_op_supervisor_everything_in_one_set(plan):
     )
 
 
+def test_op_supervisor_with_challenger(plan):
+    main.run(
+        plan,
+        {
+            "optimism_package": {
+                "chains": [
+                    {
+                        "network_params": {
+                            "network_id": "1000",
+                        }
+                    },
+                    {
+                        "network_params": {
+                            "network_id": "2000",
+                        }
+                    },
+                    {
+                        "challenger_params": {
+                            "enabled": False,
+                        },
+                        "network_params": {
+                            "network_id": "3000",
+                        },
+                    },
+                    {
+                        "network_params": {
+                            "network_id": "4000",
+                        }
+                    },
+                ],
+                "interop": {
+                    "sets": [
+                        {
+                            "name": "the-interopest-of-sets",
+                            "participants": ["1000", "2000"],
+                        },
+                        {
+                            "name": "sibling-of-the-interopest-of-sets",
+                            "participants": ["2000", "3000", "4000"],
+                        },
+                    ],
+                },
+            },
+        },
+    )
+
+    services = plan.get_services()
+    challenger_service_configs_by_name = {
+        s.name: kurtosistest.get_service_config(s.name)
+        for s in services
+        if s.name.startswith("op-challenger-")
+    }
+
+    expect.true("op-challenger-1000" in challenger_service_configs_by_name)
+    expect.true("op-challenger-2000" in challenger_service_configs_by_name)
+    expect.true("op-challenger-3000" not in challenger_service_configs_by_name)
+    expect.true("op-challenger-4000" in challenger_service_configs_by_name)
+
+    challenger_service_config1000 = challenger_service_configs_by_name[
+        "op-challenger-1000"
+    ]
+    challenger_service_config2000 = challenger_service_configs_by_name[
+        "op-challenger-2000"
+    ]
+    challenger_service_config4000 = challenger_service_configs_by_name[
+        "op-challenger-4000"
+    ]
+
+    # The first challenger should point to the first supervisor since that's the only one for the 1000 network
+    expect.true(
+        "--supervisor-rpc={0}".format(
+            "http://op-supervisor-the-interopest-of-sets:8545"
+        )
+        in challenger_service_config1000.cmd[0]
+    )
+    # The second challenger should point to the first supervisor since that's the first one of the two for the 2000 network
+    expect.true(
+        "--supervisor-rpc={0}".format(
+            "http://op-supervisor-the-interopest-of-sets:8545"
+        )
+        in challenger_service_config2000.cmd[0]
+    )
+    # The third challenger should point to the second supervisor since that's the only one for the 4000 network
+    expect.true(
+        "--supervisor-rpc={0}".format(
+            "http://op-supervisor-sibling-of-the-interopest-of-sets:8545"
+        )
+        in challenger_service_config4000.cmd[0]
+    )
+
+
 def test_create_dependency_set_filename(plan):
     expect.eq(
         op_supervisor_launcher.create_dependency_set_filename(

test/op_challenger_launcher_test.star
@@ -3,6 +3,9 @@ op_challenger_launcher = import_module(
 )
 input_parser = import_module("/src/package_io/input_parser.star")
 observability = import_module("/src/observability/observability.star")
+op_supervisor_launcher = import_module(
+    "/src/interop/op-supervisor/op_supervisor_launcher.star"
+)
 ethereum_package_constants = import_module(
     "github.com/ethpandaops/ethereum-package/src/package_io/constants.star"
 )
@@ -33,9 +36,11 @@ def test_launch_with_defaults(plan):
 
     el_context = struct(
         rpc_http_url="rpc_http_url",
+        ip_addr="1.2.3.4",
     )
     cl_context = struct(
         beacon_http_url="beacon_http_url",
+        ip_addr="4.3.2.1",
     )
     # el_context, cl_context = launch_test_el_cl(plan, parsed_input_args)
 
@@ -64,6 +69,35 @@ def test_launch_with_defaults(plan):
         challenger_private_key_mock
     )
 
+    supervisor = op_supervisor_launcher.launch(
+        plan=plan,
+        interop_set=struct(
+            enabled=True,
+            name="interop-set",
+            participants=["1000"],
+            supervisor_params=struct(
+                enabled=True,
+                image="op-supervisor:latest",
+                dependency_set=None,
+                extra_params=[],
+            ),
+        ),
+        l1_config_env_vars=l1_config_env_vars,
+        l2s=[
+            struct(
+                network_id="1000",
+                participants=[
+                    struct(
+                        el_context=el_context,
+                        cl_context=cl_context,
+                    )
+                ],
+            )
+        ],
+        jwt_file="/path/to/jwt_file",
+        observability_helper=observability_helper,
+    )
+
     op_challenger_launcher.launch(
         plan=plan,
         l2_num=l2_num,
@@ -75,7 +109,7 @@ def test_launch_with_defaults(plan):
         deployment_output=deployment_output,
         network_params=chain.network_params,
         challenger_params=chain.challenger_params,
-        interop_params=parsed_input_args.interop,
+        supervisor=supervisor,
         observability_helper=observability_helper,
     )
 
@@ -92,6 +126,6 @@ def test_launch_with_defaults(plan):
     expect.eq(
         challenger_service_config.cmd,
         [
-            "mkdir -p /data/op-challenger/op-challenger-data && op-challenger --cannon-l2-genesis=/network-configs/genesis-2151908.json --cannon-rollup-config=/network-configs/rollup-2151908.json --game-factory-address=challenger_private_key --datadir=/data/op-challenger/op-challenger-data --l1-beacon=CL_RPC_URL --l1-eth-rpc=L1_RPC_URL --l2-eth-rpc=rpc_http_url --private-key=challenger_private_key --rollup-rpc=beacon_http_url --trace-type=cannon,permissioned --metrics.enabled --metrics.addr=0.0.0.0 --metrics.port=9001 --cannon-prestates-url=https://storage.googleapis.com/oplabs-network-data/proofs/op-program/cannon"
+            "mkdir -p /data/op-challenger/op-challenger-data && op-challenger --cannon-l2-genesis=/network-configs/genesis-2151908.json --cannon-rollup-config=/network-configs/rollup-2151908.json --game-factory-address=challenger_private_key --datadir=/data/op-challenger/op-challenger-data --l1-beacon=CL_RPC_URL --l1-eth-rpc=L1_RPC_URL --l2-eth-rpc=rpc_http_url --private-key=challenger_private_key --rollup-rpc=beacon_http_url --trace-type=cannon,permissioned --metrics.enabled --metrics.addr=0.0.0.0 --metrics.port=9001 --supervisor-rpc=http://op-supervisor-interop-set:8545 --cannon-depset-config=dummy-file.json --cannon-prestates-url=https://storage.googleapis.com/oplabs-network-data/proofs/op-program/cannon"
         ],
     )